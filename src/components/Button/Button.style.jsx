f3204b79a75847274db71ca64038182e62917d5c
Justin Lim <justinlim@Jarvis-BTP.hitronhub.home>
2018-11-26 14:39:42 -0800

    Style update

--- src/components/Button/Button.style.jsx
+++ src/components/Button/Button.style.jsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
 const ButtonStyle = styled.button`
+    font-size: 12px;
     font-weight: 600;
-    line-height: ${props => props.href ? "initial" : "1"}
+    line-height: ${props => props.href ? "initial" : "1"};
     position: relative;
     cursor: pointer;
+    padding: 0 16px;
     outline: none;
     border: none;
     background-color: transparent;
