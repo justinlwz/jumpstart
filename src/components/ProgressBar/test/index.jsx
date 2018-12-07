c346da8f69d2e91e694a45eaabce46ab7c048231
Justin Lim <justinlim@Jarvis-Pro.local>
2018-12-06 21:42:30 -0800

    add progressbar render test

---
+++ src/components/ProgressBar/test/index.jsx
@@ -0,0 +1,11 @@
+import test from 'ava'
+import React from 'react'
+import render from 'react-test-renderer'
+
+import { ProgressBar } from '..'
+
+test('ProgressBar component', t => {
+    const component = <ProgressBar />
+    const tree = render.create(component).toJSON()
+    t.snapshot(tree)
+})
