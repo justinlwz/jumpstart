f148497e22ca557c6c78e475f99687919a271dd7
Justin Lim <justinlim@Jarvis-BTP.hitronhub.home>
2018-12-13 14:00:45 -0800

    update webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-    entry: ['@babel/polyfill']
+    entry: ['@babel/polyfill'],
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            }
+        ]
+    }
 }
