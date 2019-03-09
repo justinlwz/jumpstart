17a80784032abfd09aea0e79ed9d59e1e2028612
Justin Lim <justinlim@Jarvis-D7P.lan>
2019-03-08 16:53:46 -0800

    onclick prop validation

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -11,6 +11,7 @@ class Button extends PureComponent {
     radius: PropTypes.number,
     small: PropTypes.bool,
     disabled: PropTypes.bool,
+    onClick: PropTypes.func,
   }
 
   static defaultProps = {
