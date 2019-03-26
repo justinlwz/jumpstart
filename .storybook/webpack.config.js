b95cbe0fd7c07113f4f87ac49f836867ae164b45
Justin Lim <justinlim@Jarvis-D7P.lan>
2019-03-25 23:59:30 -0700

    remove polyfill

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,9 @@
 module.exports = async ({ config }) => {
-
   config.module.rules.push({
     test: /\.svg$/,
     exclude: /node_modules/,
     loaders: 'file-loader',
   });
 
-  return config;
-};
+  return config
+}
